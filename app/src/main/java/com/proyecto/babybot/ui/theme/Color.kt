82d386a0e17d54840169773eeb22044ce9d6193f
kevs <kevinyael2010@gmail.com>
2026-02-13 21:00:16 -0600

    Added some color

--- app/src/main/java/com/proyecto/babybot/ui/theme/Color.kt
+++ app/src/main/java/com/proyecto/babybot/ui/theme/Color.kt
@@ -9,3 +9,9 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+Ad
+//Paleta de colores BabyBot
+
+val BlueSkyeLight = Color(0xFFC0D7DC)
+val HardBlueText = Color(0xFF135AFF)
+val LightBlueButton = Color(0xFF648DEF)
