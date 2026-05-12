816944b7f6820a02ef5e09b955fc1923859d05ae
AndresZap98 <114139178+Danthalian98@users.noreply.github.com>
2026-05-12 00:38:08 -0600

    Despliegue
    Modificaciones hechas durante el despliegue

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,11 +26,11 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.proyecto.babybot"
+        applicationId = "com.proyectobabybot.ceti"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
